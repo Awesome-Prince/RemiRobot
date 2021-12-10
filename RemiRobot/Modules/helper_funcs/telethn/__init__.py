7a1106630b83c51c33b40fefbce1f55c31341b6c
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-12-11 01:32:08 +0530

    Baka modules

--- RemiRobot/Modules/helper_funcs/telethn/__init__.py
+++
@@ -1,9 +0,0 @@
-from RemiRobot import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES, telethn
-
-IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
-
-IMMUNE_USERS = (
-
-    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
-
-)
