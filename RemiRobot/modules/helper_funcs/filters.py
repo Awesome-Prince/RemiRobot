6894db56d2538e6c01ac107b9ba9bcd3c420e639
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-12-11 14:05:35 +0530

    RemiTheRobot

--- RemiRobot/modules/helper_funcs/filters.py
+++ RemiRobot/modules/helper_funcs/filters.py
@@ -1,6 +1,6 @@
 from RemiRobot import DEV_USERS, DRAGONS, DEMONS
 from telegram import Message
-from telegram.ext import BaseFilte
+from telegram.ext import BaseFilter
 
 
 class CustomFilters(object):
